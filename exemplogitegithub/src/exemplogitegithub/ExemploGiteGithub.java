4438fe7a2d7f672d65a6a2bfcdfb62b25e906d9e
ViniciusRafaelGoncalves <viniciusgoncalves351@gmail.com>
2023-09-03 13:12:20 -0300

    Prática: Eclipse, Java, Git e GitHub

---
+++ exemplogitegithub/src/exemplogitegithub/ExemploGiteGithub.java
@@ -0,0 +1,10 @@
+package exemplogitegithub;
+
+public class ExemploGiteGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Olá, Mundo!!!");
+	}
+
+}
